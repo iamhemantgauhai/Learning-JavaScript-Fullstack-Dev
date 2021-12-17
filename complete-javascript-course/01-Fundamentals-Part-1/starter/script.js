eae032f5fdbf6b4808b1bd722a96baeedc0c3047
Hemant Gauhai <77909457+iamhemantgauhai@users.noreply.github.com>
2021-12-18 01:29:34 +0530

    initial commit

---
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,42 @@
+/* let js = "amazing";
+// if (js === "amazing") {
+//   alert("JavaScript is Amazing!");
+// }
+
+console.log(48 + 90 - 80);
+
+let firstName = "Hemant";
+let secondName = "Gauhai";
+
+// let 3years = 3; //! Error //
+
+let Person = "Hemant";//! Bad Writing Case //
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";//? Good Variable Naming //
+
+let job1 = "programmer";
+let job2 = "teacher";//! Bad Variable Naming //
+*/
+// * 7 Primitive Data Type //
+
+//? 1.Number {decimal & integers}
+let height = 161;
+let age = 21;
+
+//? 2.String
+let myName = "Hemant";
+
+//? 3.Boolean
+let amazing = true;
+
+//? 4.Undefiend(empty-values)
+// let car = ;
+
+//? 5.Null(empty-value)
+let myClass = null;
+
+//? 6.Symbol(ES2015)
+
+//? 7.BigINT(ES2020)
+let bigNumber = 1.2698762578;
